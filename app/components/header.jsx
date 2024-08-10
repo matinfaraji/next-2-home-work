14dba57d68fda69879508755493d70b962ef66b2
matin faraji <matinfy1380@gmail.com>
2024-08-11 01:12:36 +0330

    done

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,14 +1,35 @@
-import Link from 'next/link';
+"use client";
 
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 const Header = () => {
-    return (
-        <nav>
-            <ul>
-                <li><Link href="/">Home</Link></li>
-                <li><Link href="/support">Support</Link></li>
-                <li><Link href="/store">Store</Link></li>
-                <li><Link href="/about">About</Link></li>
-            </ul>
-        </nav>
-    );
+  const pathname = usePathname();
+  const links = [
+    { title: "Home", href: "/" },
+    { title: "Support", href: "/support" },
+    { title: "Store", href: "/store" },
+    { title: "About", href: "/about" },
+  ];
+  return (
+    <header className="my-4">
+      <nav>
+        <ul className="flex items-center gap-x-5 justify-center">
+          {links.map((link, i) => (
+            <li
+              key={i}
+              className={`hover:border-b hover:cursor-pointer border-b ${
+                link.href === pathname
+                  ? "border-b-white"
+                  : "border-b-transparent"
+              } hover:border-b-white transition-all duration-300`}
+            >
+              <Link href={link.href}>{link.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 };
+
+export default Header;
--- app/page.js
+++ app/page.js
@@ -1,6 +1,3 @@
-
 export default function Home() {
-  return (
-    <p className="text-center mt-10 text-5xl">wellcome</p>
-  );
+  return <p className="text-center mt-10 text-5xl">wellcome</p>;
 }
